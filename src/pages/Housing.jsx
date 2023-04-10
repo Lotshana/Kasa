761addb077a9e52de29d4fc3c59bf1ba2102a17d
Lotshana <nahh.design@gmail.com>
2023-04-10 21:04:46 +0200

    Début CSS page Housing

--- src/pages/Housing.jsx
+++ src/pages/Housing.jsx
@@ -1,7 +1,8 @@
 import dataList from '../datas/Datas'
 //import Carousel from '../components/Carousel';
+import Collaps from '../components/Collaps';
 
-const Housing = ({title, location, host, rating, tags, description, equipment}) => {
+const Housing = ({title, location, host, rating, tags, description, equipments}) => {
     return (
         <section className="Housing">
             {/* {dataList.map((carousel) =>
@@ -11,12 +12,50 @@ const Housing = ({title, location, host, rating, tags, description, equipment}) 
                     pictures={carousel.pictures}
                 />
             )} */}
-            {dataList.map((housing) =>
+            {/*dataList.map((housing) =>
                 <div className="HousingInfos">
                     <h3>{housing.title}</h3>
                     <p>{housing.location}</p>
                 </div>
-            )}
+            )*/}
+            <div className="Carousel"></div>
+
+            <div className="Infos">
+                <div>
+                    <h1>Cozy loft on the Canal</h1>
+                    <p>Paris, Île-de-France</p>
+                </div>
+                <div className="Profile">
+                    <p>Alexandre Dumas</p>
+                    <img src="../assets/profile.jpg" alt="Photo de profil" />
+                </div>
+            </div>
+            <div>
+                <ul>
+                    <li>Cozy</li>
+                    <li>Canal</li>
+                    <li>Paris 10</li>
+                </ul>
+            </div>
+
+            <div className="Description">
+                <Collaps />
+                <Collaps />
+                {/*dataList.map(({title, content}, index) =>
+                    <Collaps
+                        key={`${index}`}
+                        title={title}
+                        content={content}
+                    />
+                )*/}
+                {/*dataList.map(({title, content}, index) =>
+                    <Collaps
+                        key={`${index}`}
+                        title={title}
+                        equipments={equipments}
+                    />
+                )*/}
+            </div>
         </section>
     )
 }
