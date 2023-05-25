03a5db72713e458f5423ad698914a036949f4f20
Lotshana <nahh.design@gmail.com>
2023-05-25 15:13:59 +0200

    Correction du header pour les liens actifs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,22 @@
 import { NavLink } from 'react-router-dom';
 import logo from '../assets/logo.svg';
+import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const Header = () => {
+    const location = useLocation();
+    const [path, setPath] = useState('/');
+
+    useEffect(() => {
+        setPath(location.pathname)
+      },[location.pathname]);
+
     return (
         <header className="Navigation">
-            <span><img src={logo} alt="Logo Kasa" /></span>
+            <span><NavLink to="/"><img src={logo} alt="Logo Kasa" /></NavLink></span>
             <ul>
-                <li><NavLink to="/">Accueil</NavLink></li>
-                <li><NavLink to="/about">À propos</NavLink></li>
+                <li><NavLink className={(path === '/' || path === '/home') ? "underline" : ""} to="/">Accueil</NavLink></li>
+                <li><NavLink className={(path === '/about') ? "underline" : ""} to="/about">À propos</NavLink></li>
             </ul>
         </header>
     )
